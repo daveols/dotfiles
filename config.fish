ed6a66ba49997716e62c62f044004654341a2917
daveols <10344370+daveols@users.noreply.github.com>
2021-01-04 15:53:42 +1100

    add fish, improve install script, clean up

---
+++ config.fish
@@ -0,0 +1,10 @@
+set -x --universal FLUTTERPATH $HOME/development/flutter
+set -x PATH $PATH $FLUTTERPATH/bin
+
+set -x --universal ANDROID_HOME $HOME/Library/Android/sdk
+set -x PATH $PATH $ANDROID_HOME/platform-tools
+
+set -x FZF_DEFAULT_COMMAND 'fd --type f'
+set -x FZF_DEFAULT_OPTS "
+  --height 20%
+"
